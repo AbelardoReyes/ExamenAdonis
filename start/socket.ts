b920d48327e1db58748c59a55f5c15c9960d19ed
Abelardo <abelardoreyes256@gmail.com>
2023-03-29 07:58:34 -0600

    sa

--- start/socket.ts
+++ start/socket.ts
@@ -16,6 +16,8 @@ Ws.io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     monitores.splice(monitores.indexOf(socket.id), 1);
     Ws.io.emit("connectedUsers", monitores);
+    Ws.io.emit("usuarios", monitores);
+
     console.log(monitores);
   });
 
@@ -28,6 +30,7 @@ Ws.io.on("connection", (socket) => {
     }
     console.log(data.id, "se agrego al arreglo: ", monitores);
     socket.emit("usuarios", monitores);
+    Ws.io.emit("usuarios", monitores);
 
   });
   Ws.io.emit("connectedUsers", monitores);
@@ -35,6 +38,11 @@ Ws.io.on("connection", (socket) => {
   console.log(monitores);
 
   socket.on("iniciar", (data) => {
+    if(orden.length<=0){
+      let error="No hay usuarios en la cola"
+      Ws.io.emit("error",error)
+    }
+
     console.log('Inicio: ', monitores[turno])
     Ws.io.emit("iniciar", data);
   });
@@ -42,22 +50,24 @@ Ws.io.on("connection", (socket) => {
 
   socket.on("monitor", (data) => {
     turno=data.turno
-    usuario=data.usuario
+    usuario=data.id
     //añadir a orden el usuario en el turno que le toca
-    orden[turno]=usuario
+    orden[turno-1]=usuario
     console.log('Orden: ', orden)
+    Ws.io.emit("usando", turno);
   });
 
   Ws.io.emit("orden", orden);
 
 
   socket.on("termino", (data) => {
-    console.log('Terminado: ', monitores[turno])
+    console.log('Terminado: ', orden[turno])
     Ws.io.emit("termino", data);
     turno++
     if (turno >= monitores.length) {
       turno = 0
     }
-    console.log('Turno: ', monitores[turno])
+    Ws.io.emit("iniciar", turno);
+    console.log('Turno: ', orden[turno])
   } );
 });
