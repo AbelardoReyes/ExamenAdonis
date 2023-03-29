d6a4f4c288f406ff775e8ebe8587c0f3f5db3731
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2023-03-29 03:19:37 -0600

    internto5

--- start/socket.ts
+++ start/socket.ts
@@ -5,21 +5,33 @@ Ws.boot()
  * Listen for incoming socket connections
  */
 const monitores: string[] = []
-let turno = 0
+let turno = 9
 Ws.io.on("connection", (socket) => {
   console.log("Conexion activa");
-
   socket.on("disconnect", () => {
     monitores.splice(monitores.indexOf(socket.id), 1);
     Ws.io.emit("connectedUsers", monitores);
     console.log(monitores);
   });
-
   console.log(monitores);
-
   Ws.io.emit("connectedUsers", monitores);
-
   socket.on("monitor", (data) => {
-    monitores.push(socket.id);
+    monitores.push(data);
   });
+
+  socket.on("inicio", (data) => {
+    if (turno == 9){turno = monitores.indexOf(data)}
+    Ws.io.emit("inicio", data);
+    console.log(turno, monitores.indexOf(data));
+  });
+
+  socket.on("termino", (data) => {
+    console.log('Terminado: ', monitores[turno])
+    Ws.io.emit("termino", data);
+    turno++
+    if (turno >= monitores.length) {
+      turno = 0
+    }
+    console.log('Turno: ', monitores[turno])
+  } );
 });
