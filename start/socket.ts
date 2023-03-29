561f0d3a8bc343e807c5902d9e221dd8f58fd0f9
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2023-03-29 02:52:34 -0600

    intento sockets 1

--- start/socket.ts
+++ start/socket.ts
@@ -4,6 +4,33 @@ Ws.boot()
 /**
  * Listen for incoming socket connections
  */
+const monitores: string[] = []
+let turno = 0
 Ws.io.on('connection', (socket) => {
   console.log('Nueva Conexión: ', socket.id)
+
+  socket.on('monitor', (data) => {
+    monitores.push(data)
+    console.log('Monitores: ', monitores)
+  })
+
+  socket.on('inicio', (data) => {
+
+    console.log('Inicio: ', monitores[turno])
+  })
+
+  socket.on('terminado', (data) => {
+    console.log('Terminado: ', monitores[turno])
+    turno++
+    if (turno >= monitores.length) {
+      turno = 0
+    }
+    console.log('Turno: ', monitores[turno])
+  })
+
+
+  socket.on('disconnect', () => {
+    monitores.splice(monitores.indexOf(socket.id), 1);
+    console.log('Desconectado: ', socket.id)
+  })
 })
